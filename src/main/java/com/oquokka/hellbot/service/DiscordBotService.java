30eab07279bf722c9a95fb711601c636fc3b1415
Daniel Bohry <danielbohry@gmail.com>
2024-06-28 23:16:51 +0200

    Extract event to class

--- src/main/java/com/oquokka/hellbot/service/DiscordBotService.java
+++ src/main/java/com/oquokka/hellbot/service/DiscordBotService.java
@@ -1,19 +1,22 @@
 package com.oquokka.hellbot.service;
 
-import com.oquokka.hellbot.event.UserActivityEvent;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceUpdateEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.events.user.update.UserUpdateActivitiesEvent;
 import net.dv8tion.jda.api.events.user.update.UserUpdateOnlineStatusEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -26,7 +29,6 @@ public class DiscordBotService extends ListenerAdapter {
     public void startBot() {
         JDABuilder.createDefault(botToken)
                 .enableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MEMBERS)
-                .addEventListeners(new UserActivityEvent())
                 .addEventListeners(this)
                 .setActivity(Activity.watching("the server"))
                 .enableCache(CacheFlag.ACTIVITY)
@@ -42,6 +44,18 @@ public class DiscordBotService extends ListenerAdapter {
     public void onUserUpdateOnlineStatus(UserUpdateOnlineStatusEvent event) {
         if (event.getNewOnlineStatus() == OnlineStatus.ONLINE) {
             log.info("{} is online", event.getUser().getName());
+        }
+    }
+
+    @Override
+    public void onUserUpdateActivities(UserUpdateActivitiesEvent event) {
+        String user = event.getUser().getName();
+        List<Activity> activities = event.getNewValue();
+        if (activities != null && !activities.isEmpty()) {
+            Activity activity = activities.get(0);
+            log.info("{} has activities: {}", user, activity.getName());
+        } else {
+            log.info("{} has no activities", user);
         }
     }
 
